6761ee7b997ba45a317726ecc163e6222d7de7cc
Andrea Corallo <andrea.corallo@arm.com>
2018-07-26 00:41:11 +0200

    license added

--- veri-kompass-mode.el
+++ veri-kompass-mode.el
@@ -1,4 +1,32 @@
-;;; -*- lexical-binding: t; -*-
+;;; veri-kompass.el --- verilog codebase navigation facility for Emacs  -*- lexical-binding:t -*-
+
+;; Copyright (C) 2018 Andrea Corallo
+
+;; Maintainer: andrea_corallo@yahoo.it
+;; Package: veri-kompass
+
+;; This file is not part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provide verilog codebase navigation facility for Emacs.
+;; Including a hierarchy sidebar and functions to follow drivers and loads
+;; within the design.
+
+;;; Code:
 
 (require 'helm)
 (require 'org)
